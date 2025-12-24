4f4632550d878fdfc6d41af997a38b59db48e40b
Pritish Raj <pritish499@gmail.com>
2025-12-24 15:49:52 +0530

    Fix logout function to clear token cookie correctly

--- backend/controllers/auth.contollers.js
+++ backend/controllers/auth.contollers.js
@@ -73,7 +73,12 @@ export const login= async(req,res)=>{
 
 export const logout= async(req,res)=>{
     try {
-        res.clearCookie("token")
+        res.clearCookie("token",token,{
+            httpOnly:true,
+            sameSite:"None",
+            secure:true,
+            maxAge:7*24*60*60*1000
+        })
         return res.status(200).json({message:"Logout successfully"})
     } catch (error) {
         console.log(error)
